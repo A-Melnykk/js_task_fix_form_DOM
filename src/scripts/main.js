f28e80395784c5c4e33b742db76f5cc67eca089f
A-Melnykk <969696245n@gmail.com>
2025-11-25 00:48:01 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,15 +6,17 @@ forms.forEach((form) => {
   const inputs = form.querySelectorAll('input');
 
   inputs.forEach((input) => {
-    const inputName = input.name;
+    const inputName = input.name.replace(/-/g, ' ');
 
     const label = document.createElement('label');
 
     label.classList.add('field-label');
     label.setAttribute('for', input.id);
+
     label.textContent = inputName.toUpperCase();
 
-    input.placeholder = inputName[0].toUpperCase() + inputName.slice(1);
+    input.placeholder =
+      inputName[0].toUpperCase() + inputName.slice(1).toLowerCase();
 
     input.parentElement.prepend(label);
   });
